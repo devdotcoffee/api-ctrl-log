2c7f75316cc06955caebd405bc260ead3d8f11fe
Erick Oliveira <erick_santos00@outlook.com>
2020-06-23 13:43:21 -0300

    Constantes para configuração de banco de dados

---
+++ app/Config.php
@@ -0,0 +1,11 @@
+<?php
+
+# Defining constants for database config
+define('DB_CONFIG', array(
+    'DRIVER'    => 'mysql',
+    'DBNAME'    => 'projetologistica',
+    'HOST'      => 'localhost',
+    'USER'      => 'root',
+    'PASSWORD'  => "",
+    "PORT"      => "3306"
+), FALSE);
