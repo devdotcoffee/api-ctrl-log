2b0c6bd4cd25c8cce748e00910d38bdddc6dc1af
Erick Oliveira <erick_santos00@outlook.com>
2020-06-25 13:31:59 -0300

    Terminando métodos de teste

--- app/Tests/Config/DataDefinitionTest.php
+++ app/Tests/Config/DataDefinitionTest.php
@@ -4,16 +4,57 @@ namespace App\Tests\Config;
 
 use PHPUnit\Framework\TestCase;
 use App\Config\DataDefinition;
+use App\Config\Database;
 
 class DataDefinitionTest extends TestCase {
 
-    public function testCreateTables(): void 
+    private $connection;
+
+    protected function setUp(): void
     {
-        DataDefinition::createTables();
+        $this->connection = Database::connect();
     }
 
+    /**
+     * @group createTable
+     * @group createAllTables
+     */
+    public function testCreateTables(): void 
+    {
+        $database = new DataDefinition();
+        $database->createTables();
+
+        $query = "SELECT table_name FROM information_schema.tables WHERE table_schema = :database";
+        $query = $this->connection->prepare($query);
+        $query->bindValue(':database', DB_CONFIG['DBNAME']);
+        $query->execute();
+        $rows = $query->rowCount();
+
+        $tables = $database->countTables();
+        
+        $this->assertEquals($tables, $rows);
+    }
+
+    /**
+     * @group dropTable
+     * @group dropAllTables
+     */
     public function testDropTables(): void 
     {
-        DataDefinition::dropTables();
+        $database = new DataDefinition();
+        $database->dropTables();
+
+        $query = "SELECT table_name FROM information_schema.tables WHERE table_schema = :database";
+        $query = $this->connection->prepare($query);
+        $query->bindValue(':database', DB_CONFIG['DBNAME']);
+        $query->execute();
+        $rows = $query->rowCount();
+        
+        $this->assertEquals(0, $rows);
+    }
+
+    protected function tearDown(): void
+    {
+        $this->connection = null;
     }
 }
