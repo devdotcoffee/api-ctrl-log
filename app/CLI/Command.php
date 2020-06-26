cb2f9ba143a17e2411dffadeccf7e3d645abbc86
Erick <erick_santos00@outlook.com>
2020-06-26 13:43:35 -0300

    Adicionando métodos para criar, deletar e refazer tabelas

--- app/CLI/Command.php
+++ app/CLI/Command.php
@@ -2,6 +2,8 @@
 
 namespace App\CLI;
 
+use App\Config\DataDefinition;
+
 /**
  * @author Erick O. dos Santos
  */
@@ -14,20 +16,66 @@ class Command {
     ];
 
     private static $options = [
-        '--seed' => 'Incrementa dados em todas as tabelas criadas'
+        '--seed' => "Incrementa dados em todas as tabelas criadas.\n\t\t\tSó deve ser usada com [createTables] ou [refresh]."
     ];
 
     public static function help(): void
     {
-        echo("\nUso:\n\n");
-        echo("\tdb [comando] [option]\n\n");
-        echo("Comandos:\n\n");
-        
+        echo("\nUsage:\n");
+        echo("\tdb [command] [option]\n\n");
+        echo("Commands:\n");
+        self::getCommands();
+        echo("Options:\n");
+        self::getOptions();
+    }
+
+    private static function getCommands(): void
+    {
         $keys   = array_keys(self::$commands);
         $values = array_values(self::$commands);
 
         for ($i = 0; $i <= (count(self::$commands) - 1); $i++) {
             echo("\t" . $keys[$i] . "\t\t" . $values[$i] . "\n");
         }
+    }
+
+    private static function getOptions(): void 
+    {
+        $keys   = array_keys(self::$options);
+        $values = array_values(self::$options);
+
+        for ($i = 0; $i <= (count(self::$options) - 1); $i++) {
+            echo("\t" . $keys[$i] . "\t\t" . $values[$i] . "\n");
+        }
+    }
+
+    public static function createTables(): void 
+    {
+        $data = new DataDefinition();
+
+        echo("Criando Tabelas...\n");
+        $data->createTables();
+        echo("Tabelas criadas com sucesso.");
+    }
+
+    public static function dropTables(): void
+    {
+        $data = new DataDefinition();
+
+        echo("Apagando Tabelas...\n");
+        $data->dropTables();
+        echo("Tabelas deletadas com sucesso.");
+    }
+
+    public static function refresh(): void 
+    {
+        $data = new DataDefinition();
+
+        echo("Apagando Tabelas...\n");
+        $data->dropTables();
+        echo("Criando Tabelas...\n");
+        $data->createTables();
+        echo("Tabelas criadas com sucesso.");
+
     }
 }
